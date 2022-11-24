30a9f83e8c5928caaf03129b027046e53b947bf4
ReyzOnly <115127345+ReyzOnly@users.noreply.github.com>
2022-11-24 12:57:50 +0700

    Create Variabel.java
    Soal no 5 uts

---
+++ Variabel.java
@@ -0,0 +1,11 @@
+public class Main
+{
+	public static void main(String[] args) {
+		String nama = "Reva Yulian Satria";
+		Integer tahun = 2004;
+		
+		System.out.println("Assalamualaikum");
+		System.out.println("Nama saya : "+ nama);
+		System.out.println("Saya Lahir di Tahun : "+ tahun);
+	}
+}
